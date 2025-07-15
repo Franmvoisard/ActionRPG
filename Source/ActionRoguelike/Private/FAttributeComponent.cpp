b40d020a99fce2872632f400aef7ab1a9d8dd785
Franco Voisard <franm.voisard@gmail.com>
2025-07-14 21:24:31 -0400

    Modify Attribute component to cap health and prevent healing if player health is max health

--- Source/ActionRoguelike/Private/FAttributeComponent.cpp
+++ Source/ActionRoguelike/Private/FAttributeComponent.cpp
@@ -6,6 +6,7 @@
 UFAttributeComponent::UFAttributeComponent()
 {
 	Health = 100.0f;
+	MaxHealth = 100.0f;
 }
 
 bool UFAttributeComponent::IsAlive() const
@@ -15,7 +16,19 @@ bool UFAttributeComponent::IsAlive() const
 
 bool UFAttributeComponent::ApplyHealthChange(float Delta)
 {
+	if (Delta > 0.0f) //Healing
+	{
+		//Ignore healing if our health is already above the MaxHealth
+		if (Health >= MaxHealth) return false;
+	}
+	else
+	{
+		//Ignore damage if our health is already 0
+		if (Health <= 0.0f) return false;
+	}
+	
 	Health += Delta;
+	Health = FMath::Clamp(Health, 0.0f, MaxHealth);
 	OnHealthChange.Broadcast(nullptr, this, Health, Delta);
 	return true;
 }

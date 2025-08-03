547e53feefb7998a2b3aafbc231107d2f689130e
Franco Voisard <franm.voisard@gmail.com>
2025-08-02 23:30:00 -0400

    Removed unused includes

--- Source/ActionRoguelike/Private/AI/FAIController.cpp
+++ Source/ActionRoguelike/Private/AI/FAIController.cpp
@@ -1,10 +1,6 @@
 // Public Domain - 2025 Franco Voisard. This code is provided for skill and knowledge demo purposes. No rights reserved. Use freely.
 
-
 #include "AI/FAIController.h"
-
-#include "BehaviorTree/BlackboardComponent.h"
-#include "Kismet/GameplayStatics.h"
 
 void AFAIController::BeginPlay()
 {

73725399bc4f47bc602d35211e4796f7ded1f763
Franco Voisard <franm.voisard@gmail.com>
2025-08-17 02:24:02 -0400

    Create custom player state

---
+++ Source/ActionRoguelike/Private/FPlayerState.cpp
@@ -0,0 +1,5 @@
+// Public Domain - 2025 Franco Voisard. This code is provided for skill and knowledge demo purposes. No rights reserved. Use freely.
+
+
+#include "FPlayerState.h"
+
---
+++ Source/ActionRoguelike/Public/FPlayerState.h
@@ -0,0 +1,17 @@
+// Public Domain - 2025 Franco Voisard. This code is provided for skill and knowledge demo purposes. No rights reserved. Use freely.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "FPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API AFPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
